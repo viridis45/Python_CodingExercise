b346b38c609bfec01e10249e726942eebd80af52
khoh404 <khoh@obelab.com>
2021-03-17 17:38:35 +0900

    03172020

--- 03172020.py
+++ 03172020.py
@@ -1,10 +1,9 @@
-https://programmers.co.kr/learn/courses/30/lessons/12928?language=python3
+#https://programmers.co.kr/learn/courses/30/lessons/12928?language=python3
 
 
 def solution(n):
-    answer = sum(set( [a for a in range(1,n+1) if n%(a)==0]))
-    return answer
-
+    return sum(set( [a for a in range(1,n+1) if n%a==0]))
+    #return num + sum( [a for a in range(1,(n/2)+1) if n%(a)==0])
 
 
 if __name__ == "__main__":
