4cb84f9878d65ec94de9686de6c501c48ba0aef2
khoh404 <viridis404@gmail.com>
2021-03-22 16:27:18 +0900

    comfortable

---
+++ 20210322.py
@@ -0,0 +1,10 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12934?language=python3
+
+
+def solution(n : int) -> int:
+    inp2 = n**(1/2)
+    return int((inp2+1)**2) if inp2 == int(inp2) else -1
+
+
+if __name__ == "__main__":
+    solution(121)
