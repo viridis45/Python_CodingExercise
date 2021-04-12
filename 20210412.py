dd5a1f43f3baf2b63e52c1a70e88f916cd4018c2
khoh404 <viridis404@gmail.com>
2021-04-12 15:29:25 +0900

    fun

---
+++ 20210412.py
@@ -0,0 +1,26 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42583
+
+
+def solution(bridge_length, weight, truck_weights):
+    br, ca, wg = bridge_length, weight, truck_weights
+    totwgt, lastcarnum,carnumOnBg, crs,ct = 0,0,0,[],0
+    totlen = len(wg)
+    while lastcarnum<totlen:
+        if carnumOnBg==br: # when the bridge is full
+            totwgt-=crs.pop(0)
+            carnumOnBg-=1
+        if totwgt+wg[lastcarnum] <= ca:
+            totwgt+=wg[lastcarnum]
+            crs.append(wg[lastcarnum])
+            lastcarnum+=1
+            carnumOnBg+=1
+        else: # filling up the bridge
+            carnumOnBg+=1
+            crs.append(0) # 0 is count as a 'car', but as a placeholder.
+        ct+=1
+    return ct+br
+
+
+if __name__ == '__main__':
+    solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]) #110
+    #  2,10,[7,4,5,6], 8
