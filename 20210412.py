37eef81122bc14c7887272b4436819b1594cba1f
khoh404 <viridis404@gmail.com>
2021-04-12 15:38:19 +0900

    fun

--- 20210412.py
+++ 20210412.py
@@ -5,11 +5,11 @@ def solution(bridge_length, weight, truck_weights):
     br, ca, wg = bridge_length, weight, truck_weights
     totwgt, lastcarnum,carnumOnBg, crs,ct = 0,0,0,[],0
     totlen = len(wg)
-    while lastcarnum<totlen:
+    while lastcarnum<totlen: # counting till the last car enters the bridge
         if carnumOnBg==br: # when the bridge is full
-            totwgt-=crs.pop(0)
+            totwgt-=crs.pop(0) # first car exits
             carnumOnBg-=1
-        if totwgt+wg[lastcarnum] <= ca:
+        if totwgt+wg[lastcarnum] <= ca: #if bridge can hold the next car weight
             totwgt+=wg[lastcarnum]
             crs.append(wg[lastcarnum])
             lastcarnum+=1
@@ -18,7 +18,7 @@ def solution(bridge_length, weight, truck_weights):
             carnumOnBg+=1
             crs.append(0) # 0 is count as a 'car', but as a placeholder.
         ct+=1
-    return ct+br
+    return ct+br # counting till the last car exits the bridge
 
 
 if __name__ == '__main__':
